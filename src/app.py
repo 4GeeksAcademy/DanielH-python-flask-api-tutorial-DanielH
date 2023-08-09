429bba8fd622995927ac555d76a3730ca2b48614
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-08-09 19:00:08 +0000

    todo ok

--- src/app.py
+++ src/app.py
@@ -7,23 +7,44 @@ todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
 ]
-# danielh
-# @app.route('/todos', methods=['GET'])
-# def hello_world():
-#     json_text = jsonify(todos)
 
-#     return  json_text
+# danielh
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return  json_text
+
 
 @app.route('/todos', methods=['POST'])
-def add_new_todo():
+def add_new_todo():    
     request_body = request.json
+    todos.append(request_body)
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    return jsonify(todos)
 
 
 
 
 
+# @app.route('/todos/<int:position>', methods=['DELETE'])
+# def delete_todo(position):
+#     print("This is the position to delete: ", position)
+#     if position < len(todos):
+#         del todos[position]
+#         return jsonify({"message": "Eliminacion correcta"})
+#     else:
+#         return jsonify({"error": "Posicion fuera de rango"}), 400
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ", position)
+    if 0 <= position < len(todos):
+        del todos[position]
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Invalid position"}), 400
 
 
 
